ea2ac5fd19bdf1ba22b5da8a60135df9117954fe
russstorms <rstorms90@gmail.com>
2025-02-14 03:48:08 -0500

    Update nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { scrollToSection } from '../../helpers/scrollToSection';
 import './Navbar.css';
 
@@ -11,22 +11,42 @@ const NAV_LINKS = [
 
 const Navbar = () => {
   const navbarRef = useRef(null);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [sticky, setSticky] = useState(false);
 
   useEffect(() => {
     const navbar = navbarRef.current;
     if (!navbar) return;
 
     const handleScroll = () => {
-      navbar.classList.toggle('sticky', window.scrollY > navbar.offsetTop);
+      const currentScrollY = window.scrollY;
+
+      // Check scroll direction
+      if (currentScrollY > lastScrollY && currentScrollY > navbar.offsetTop) {
+        // Scrolling down and past navbar
+        setSticky(true);
+      } else if (
+        currentScrollY < lastScrollY &&
+        currentScrollY < navbar.offsetTop
+      ) {
+        // Scrolling up and within original position
+        setSticky(false);
+      }
+
+      setLastScrollY(currentScrollY);
     };
 
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+  }, [lastScrollY]);
 
   return (
     <div className="nav-wrapper">
-      <section className="Navbar" id="navbar" ref={navbarRef}>
+      <section
+        className={`Navbar ${sticky ? 'sticky' : ''}`}
+        id="navbar"
+        ref={navbarRef}
+      >
         <div className="nav-neon-border" />
         <nav className="clipped-nav">
           {NAV_LINKS.map(({ label, section }) => (
